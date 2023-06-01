2995c352ce82452075a505efaf653e42341dd6b3
nikeedev <nikita.gon.dev@gmail.com>
2023-06-01 13:05:57 +0200

    updates

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
 
 // Write the alphabet from lower to upper case
-let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-let alphabet_len = alphabet.length;
+// let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+// let alphabet_len = alphabet.length;
 
-let websocket = new WebSocket("wss://clouddata.turbowarp.org/", ["User-Agent"]);
+let input_data = document.getElementById("input-data"), send_button = document.getElementById("send-button"), project_id_used = document.getElementById("project-id-used");
 
-websocket.onerror = (event) => {
+const project_id = 859836142;
+
+let wss = new WebSocket("wss://clouddata.scratch.mit.edu");
+
+console.log()
+
+wss.onerror = (event) => {
     console.log("Websocket error");
     console.log(event);
 }
 
-let input_data = document.getElementById("input-data"), send_button = document.getElementById("send-button"), project_id_used = document.getElementById("project-id-used");
+const variable_name = "message";
 
-const project_id = 777954330;
-
-websocket.onopen = (event) => {
+wss.onopen = async (event) => {
     console.log("Connection open");
     console.log(event);
 
-    project_id_used.innerHTML += '<a style="text-decoration: none;" href="https://turbowarp.org/'+ project_id +'" target="_blank"><code id="project-id" style="background-color:rgb(224, 224, 224); color: rgb(31, 18, 18);">turbowarp.org/'+ project_id +'</code></a>';
+    project_id_used.innerHTML += `<a style="text-decoration: none;" href="https://scratch.mit.edu/projects/${project_id}" target="_blank"><code id="project-id" style="background-color:rgb(224, 224, 224); color: rgb(31, 18, 18);">scratch.mit.edu/projects/${project_id}</code></a>`;
 
     /// Handshake request \
-    websocket.send(JSON.stringify({ "method": "handshake", "user": "nikeedev", "project_id": project_id }));
+    await wss.send(JSON.stringify({ "method": "handshake", "user": "nikeedev", "project_id": project_id }));
     /// Handshake request /
 
 
-    setTimeout(function() {
-        websocket.send(JSON.stringify({ "method": "set", "project_id": project_id, "user": "nikeedev", "name": "☁ cloud", "value": 7 }));
+    setTimeout(async () => {
+        await wss.send(JSON.stringify({ "method": "set", "project_id": project_id, "user": "nikeedev", "name": "☁ " + variable_name, "value": 7 }));
     }, 500);
 
     
     setInterval(() => {
-        send_button.onclick = (event) => {
-            websocket.send(JSON.stringify({ "method": "set", "project_id": project_id, "user": "nikeedev", "name": "☁ cloud", "value": input_data.value }));
+        send_button.onclick = async (event) => {
+            await wss.send(JSON.stringify({ "method": "set", "project_id": project_id, "user": "nikeedev", "name": "☁ " + variable_name, "value": input_data.value }));
             console.log("Value " + input_data.value + " sent");
         }
     }, 1000);
@@ -42,15 +46,15 @@ websocket.onopen = (event) => {
 
 }
 
-websocket.onmessage = (event) => {
+wss.onmessage = (event) => {
     console.log(event);
     
     setInterval(() => {
         console.log("Receiving messages:", event.data);
-    }, 1000);
+    }, 500);
 }
 
-websocket.onclose = (event) => {
+wss.onclose = (event) => {
     console.log(event);
     console.log("Closed connection")
 }
