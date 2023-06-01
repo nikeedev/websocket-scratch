3fad92adebb56fadf09bcc283e716d9af5f1d3fe
nikeedev <nikita.gon.dev@gmail.com>
2023-06-01 13:45:31 +0200

    updates

--- script.js
+++ script.js
@@ -3,9 +3,11 @@
 // let alphabet_len = alphabet.length;
 
 
-let input_data = document.getElementById("input-data"), send_button = document.getElementById("send-button"), project_id_used = document.getElementById("project-id-used");
+let input_data = document.getElementById("input-data"),
+send_button = document.getElementById("send-button"),
+project_id_used = document.getElementById("project-id-used");
 
-const project_id = 859836142;
+const project_id = "859836142";
 
 let wss = new WebSocket("wss://clouddata.scratch.mit.edu");
 
@@ -25,22 +27,21 @@ wss.onopen = async (event) => {
     project_id_used.innerHTML += `<a style="text-decoration: none;" href="https://scratch.mit.edu/projects/${project_id}" target="_blank"><code id="project-id" style="background-color:rgb(224, 224, 224); color: rgb(31, 18, 18);">scratch.mit.edu/projects/${project_id}</code></a>`;
 
     /// Handshake request \
-    await wss.send(JSON.stringify({ "method": "handshake", "user": "nikeedev", "project_id": project_id }));
+    await wss.send(JSON.stringify({ "method": "handshake", "user": "nikeedev", "project_id": project_id }) + "\n");
     /// Handshake request /
-
-
-    setTimeout(async () => {
-        await wss.send(JSON.stringify({ "method": "set", "project_id": project_id, "user": "nikeedev", "name": "☁ " + variable_name, "value": 7 }));
-    }, 500);
-
     
-    setInterval(() => {
-        send_button.onclick = async (event) => {
-            await wss.send(JSON.stringify({ "method": "set", "project_id": project_id, "user": "nikeedev", "name": "☁ " + variable_name, "value": input_data.value }));
-            console.log("Value " + input_data.value + " sent");
-        }
-    }, 1000);
-    
+    await new Promise((r) => setTimeout(r, 100));
+
+    send_button.onclick = async (event) => {
+        await wss.send(JSON.stringify({
+            "method": "set",
+            "project_id": project_id,
+            "user": "nikeedev",
+            "name": "☁ " + variable_name,
+            "value": input_data.value
+            }) + "\n");
+        console.log("Value " + input_data.value + " sent");
+    }
 
     console.log("Sent the message");
 
@@ -54,24 +55,4 @@ wss.onmessage = (event) => {
     }, 500);
 }
 
-wss.onclose = (event) => {
-    console.log(event);
-    console.log("Closed connection")
-}
-
-function update() {
-    variable_name = name_data.value;
-    task = task_data.value;
-    // console.log("Var name: " + variable_name)
-
-    requestAnimationFrame(update);
-}
-requestAnimationFrame(update);
-
-send_button.onclick = (event) => {
-    if (task == "rename") {
-        JSON.stringify({ "method": "rename", "name": "☁ " + variable_name, "value": input_data.value });
-    } 
-    websocket.send();
-    console.log("Value " + input_data.value + " sent");
-}
+wss.onclose = console.error;
